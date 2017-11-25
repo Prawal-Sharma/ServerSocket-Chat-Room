c7e2045591bc119dbb8c059cb8ac03394a574854
Eric Su <ericsu@Erics-MacBook-Pro.local>
2017-11-25 01:33:01 -0600

    added starter files

---
+++ src/assignment7/Header.java
@@ -0,0 +1,16 @@
+/* Chat Program <MyClass.java>
+ * EE422C Project 7 submission by
+ * Replace <...> with your actual data.
+ * <Prawal Sharma>
+ * <ps27933>
+ * <16280>
+ * <Eric Su>
+ * <es34826>
+ * <16275>
+ * Group on Canvas:10
+ * Slip days used: <1>
+ * Fall 2017
+ * GitHub URL:https://github.com/EE422C-F17-HW3/422c-proj7-F17-422c-pr7-f17-pair10
+ */
+
+package assignment7;
