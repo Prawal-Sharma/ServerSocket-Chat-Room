70c0b18c1f4201e0faab39fe945c47b7df7c16a5
Eric Su <ericsu@wireless-10-148-8-153.public.utexas.edu>
2017-11-27 22:50:12 -0600

    added in class diagram

--- src/assignment7/ClientMain.java
+++ src/assignment7/ClientMain.java
@@ -14,16 +14,20 @@
  */
 package assignment7;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-
-
-
-
-public final class ClientMain 
+public final class ClientMain extends Application
 {
 	
 	public static void main(String[] args) 
 	{
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
 		
 	}
 	
