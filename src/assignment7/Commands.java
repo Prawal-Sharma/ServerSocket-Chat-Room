cb1e5863300b51aab02a7a9ed09ccaaafe7c0f13
Eric Su <ericsu@wireless-10-147-252-72.public.utexas.edu>
2017-11-29 17:25:39 -0600

    added some standard commands and ports

---
+++ src/assignment7/Commands.java
@@ -0,0 +1,30 @@
+/* Chat Program <MyClass.java>
+ * EE422C Project 7 submission by
+ * Replace <...> with your actual data.
+ * <Prawal Sharma>
+ * <ps27933>
+ * <16280>
+ * <Eric Su>
+ * <es34826>
+ * <16275>
+ * Group on Canvas:10
+ * Slip days used: <1>
+ * Fall 2017
+ * GitHub URL:https://github.com/EE422C-F17-HW3/422c-proj7-F17-422c-pr7-f17-pair10
+ */
+
+package assignment7;
+
+
+//add as we go
+
+public final class Commands{
+	public static final String InvalidUsername = "*&$invalusername";
+	public static final String MakeUser = "*&$makeusr";
+	public static final String ChangePassword = "*&$changepswd";
+	
+	
+	
+	
+	
+}
