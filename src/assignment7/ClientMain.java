72be35876695b3aaa3480041ee58631420755209
Eric Su <ericsu@Erics-MacBook-Pro.local>
2017-12-03 16:58:28 -0600

    finished encryption, added in all javafx elements

--- src/assignment7/ClientMain.java
+++ src/assignment7/ClientMain.java
@@ -21,9 +21,18 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public final class ClientMain extends Application
@@ -37,14 +46,49 @@ public final class ClientMain extends Application
 	private static BufferedReader chat_reader;
 	
 	private static ArrayList<String> friends= new ArrayList<String>();
+	private static ArrayList<String> groupchatmembers = new ArrayList<String>();
 	
+	private static Scene signinScene;
+	private static Scene chatScene;
+	private static GridPane scenePane ;
+	private static GridPane contolpane ;
+	private static TextArea chatArea ;
+	private static TextField chatField ;
+	private static TextField friendField;
+	private static TextField changepasswordField;
+	private static Button changepasswordButton;
+	private static Button addfriendButton ;
+	private static Button closechatButton;
+	private static Button sendButton ;
+	private static Button startchatButton;
+	private static Button exitButton ;
+	private static Button acceptButton ;
+	
+	private static Button denyButton ;
+	private static GridPane friendPane ;
+	private static GridPane requestPane;
+	private static GridPane chatPane;
+	private static HBox changePassHBox ;
+	private static VBox controlVBox ;
+	
+	private static GridPane signinPane ;
+	private static String userName;
+	private static Label usernameLabel;
+	private static Label passwordLabel;
+	private static TextField usernameField ;
+	private static TextField passwordField ;
+	private static Button loginButton ;
+	private static Button createButton ;
+	private static VBox usernameVBox ;
+	private static VBox passwordVBox ;
+	private static HBox signinHBox ;
+	private static HBox newuserHBox ;
+	
+	static Timeline listener;
 	private static int encryptionoffset = 1;
 	
 	private static String command;
 	private static String parameter;
-	
-
-	private static TextArea chatArea = new TextArea();
 	private static String activeconversation;
 	
 	
@@ -56,13 +100,49 @@ public final class ClientMain extends Application
 	{
 		// TODO Auto-generated method stub
 		
+		initjavafx();
+		
+		primaryStage.setScene(signinScene);
+		primaryStage.show();
 		
 		
 		
-		
-		
-		
-		
+	}
+	private void initjavafx()
+	{
+		scenePane = new GridPane();
+		contolpane = new GridPane();
+		chatArea = new TextArea();
+		chatField = new TextField();
+		friendField = new TextField();
+		changepasswordField = new TextField();
+		changepasswordButton = new Button("Change Password");
+		addfriendButton = new Button("Add Friend");
+		closechatButton = new Button("Close Chat");
+		sendButton = new Button("Send");
+		startchatButton = new Button("Start Chat");
+		exitButton = new Button("Sign out");
+		acceptButton = new Button("Accep Friend Requestt");
+		denyButton = new Button("Deny Friend Request");
+		friendPane = new GridPane();
+		requestPane = new GridPane();
+		chatPane = new GridPane();
+		changePassHBox = new HBox();
+		controlVBox = new VBox();
+		signinPane = new GridPane();
+		usernameLabel = new Label("Username:");
+		passwordLabel = new Label("Password:");
+		usernameField = new TextField();
+		passwordField = new TextField();
+		loginButton = new Button("Sign In");
+		createButton = new Button("New User");
+		usernameVBox = new VBox();
+		passwordVBox = new VBox();
+		signinHBox = new HBox();
+		newuserHBox = new HBox();
+		signinScene=new Scene(signinPane );
+		contolpane.add(controlVBox, 0,0 );
+		contolpane.add(chatPane, 1,0);
 	}
 	/**
 	 * setup  socket
@@ -87,9 +167,6 @@ public final class ClientMain extends Application
 		catch(Exception e1){e1.printStackTrace();}
 		socketconnected=true;
 	}
-	
-	
-	
 	
 	/**
 	 * listen to server commands
@@ -155,11 +232,9 @@ public final class ClientMain extends Application
 		}
 	}
 
-	private static String deocde(String parameter2) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/**
+	 * adds friend to friend list
+	 */
 	private static void addfriend() {
 		// TODO Auto-generated method stub
 		
@@ -167,16 +242,16 @@ public final class ClientMain extends Application
 	private static String encode(String str, int offset)
 	{
 		
+		String retstr= new String (Base64.getUrlEncoder().encodeToString(str.getBytes()) );
 		
-		
-		return str;
+		return retstr;
 		
 	}
 	private static String decode(String str, int offset)
 	{
 		
-		
-		return str;
+		String retstr= new String( Base64.getUrlDecoder().decode(str));
+		return retstr;
 		
 	}
 	
