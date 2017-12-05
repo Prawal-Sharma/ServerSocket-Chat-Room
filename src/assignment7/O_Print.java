c1730f8866f92efc8a526319311275ec006f319f
Eric Su <ericsu@wireless-10-147-64-32.public.utexas.edu>
2017-12-05 14:25:41 -0600

    added headers

--- src/assignment7/O_Print.java
+++ src/assignment7/O_Print.java
@@ -1,3 +1,18 @@
+/* Chat Program <MyClass.java>
+ * EE422C Project 7 submission by
+ * Replace <...> with your actual data.
+ * <Prawal Sharma>
+ * <ps27933>
+ * <16280>
+ * <Eric Su>
+ * <es34826>
+ * <16275>
+ * Group on Canvas:10
+ * Slip days used: <1>
+ * Fall 2017
+ * GitHub URL:https://github.com/EE422C-F17-HW3/422c-proj7-F17-422c-pr7-f17-pair10
+ */
+
 package assignment7;
 import java.io.*;
 import java.net.*;
